7ff6208ea7fde8032665893ddeb6cf1df3acc7d3
Michael <michaelflexzy11@gmail.com>
2026-07-24 16:13:24 +0100

    Made the time period labels dynamic

--- routes/api.js
+++ routes/api.js
@@ -5,14 +5,20 @@ const router = Router();
 const colours = ['bg-orange-300', 'bg-blue-300', 'bg-pink-400', 'bg-green-400', 'bg-purple-700', 'bg-yellow-300'];
 
 router.get(['/', '/daily', '/weekly', '/monthly'], (req, res) => {
-  
-  const timeframe = req.path.replace('/', '') || 'daily';
+  const timeframe = req.path.replace(/\//g, '') || 'daily';
+
+  const timeframeLabels = {
+    daily: 'Yesterday',
+    weekly: 'Last Week',
+    monthly: 'Last Month'
+  };
 
   try {
     res.render("index", {
       data: localData,
       bgcolour: colours,
-      timeframe: timeframe,
+      timeframe,
+      label: timeframeLabels[timeframe] || 'Yesterday',
       error: null,
     });
   } catch (error) {
