f15dac643aeb9d99995d95dac6a4627dce1fcaeb
Michael <michaelflexzy11@gmail.com>
2026-07-24 13:46:53 +0100

    Initial commit

---
+++ routes/api.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import localData from "../services/data.json" with { type: "json" };
+
+const router = Router();
+const colours = ['bg-orange-300', 'bg-blue-300', 'bg-pink-400', 'bg-green-400', 'bg-purple-700', 'bg-yellow-300'];
+
+router.get(['/', '/daily', '/weekly', '/monthly'], (req, res) => {
+  
+  const timeframe = req.path.replace('/', '') || 'daily';
+
+  try {
+    res.render("index", {
+      data: localData,
+      bgcolour: colours,
+      timeframe: timeframe,
+      error: null,
+    });
+  } catch (error) {
+    console.error('Failed to render template:', error.message);
+    res.status(500).send("Template rendering error");
+  }
+});
+
+export default router;
---
+++ server.js
@@ -0,0 +1,66 @@
+import 'dotenv/config';
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import compression from 'compression';
+import methodOverride from 'method-override';
+import { v4 as uuidv4 } from 'uuid';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import apiRouter from './routes/api.js'
+
+const app = express();
+const port = process.env.PORT || 3000;
+const nodeEnv = process.env.NODE_ENV || 'development';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(helmet({ contentSecurityPolicy: false }));
+app.use(compression());
+app.use(morgan('dev'));
+app.use(methodOverride('_method'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.locals.req = req;
+    res.locals.hasAll = (...keys) => keys.every(key => key in res.locals);
+    res.locals.hasAny = (...keys) => keys.some(key => key in res.locals);
+    next();
+});
+
+app.use('/', apiRouter);
+
+app.use((req, res) => {
+    res.status(404).render('index', { data: null, bgcolour:null, error: '404 — Page not found' });
+});
+
+app.use((err, req, res, next) => {
+    if (nodeEnv === 'development') {
+        console.error(err.stack);
+    } else {
+        console.error(`[Error]: ${err.message}`);
+    }
+
+    const statusCode = err.status || err.statusCode || 500;
+    
+    const clientMessage = nodeEnv === 'development' 
+        ? err.response?.data?.error || err.message 
+        : 'Internal Server Error';
+
+    res.status(statusCode).render('index', { 
+        data: null,
+        bgcolour: null,
+        error: `${statusCode} — ${clientMessage}` 
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server online on port ${port} (${nodeEnv})`);
+});
